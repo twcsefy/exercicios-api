1090c4e461873088ce1df11303bcd3b165318773
twcsefy <mariasefora87@gmail.com>
2023-08-01 10:25:25 -0300

    Exercicio dois

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('exercicio/um', 
 [ExercicioUmController::class, 'multiplicar']);
+
+Route::post('exercicio/dois', 
+[ExercicioDoisController::class, 'retornarMaior']);
